e8950038d6506cc0b06e891f2c4bffd14e1262d1
Kevin Oliveira <55069829+klooke@users.noreply.github.com>
2024-05-27 20:00:54 -0300

    feat: show/hide new task form

--- src/main.js
+++ src/main.js
@@ -1,2 +1,22 @@
+const newTaskForm = $("#form-item");
+const taskDescriptionInput = $("#task-description");
+const showTaskFormBtn = $("#show-task-form");
 $(document).ready(function () {
+	showTaskFormBtn.on("click", () => setVisibleNewTaskForm(true));
+	newTaskForm.on("submit", (event) => {
+		event.preventDefault();
+		setVisibleNewTaskForm(false);
+	});
+	newTaskForm.on("reset", () => setVisibleNewTaskForm(false));
 });
+
+function setVisibleNewTaskForm(isVisible) {
+	if (isVisible) {
+		newTaskForm.show();
+		taskDescriptionInput.trigger("focus");
+	} else {
+		newTaskForm.hide();
+		taskDescriptionInput.val("");
+	}
+}
+}
